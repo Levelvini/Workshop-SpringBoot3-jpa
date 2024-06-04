2386365957583b241695e47421618dc7c23763e0
Levelvini <viniciusfc29@gmail.com>
2024-06-04 09:53:40 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/LevelVini/course/CourseApplication.java
+++ src/main/java/com/LevelVini/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.LevelVini.course;
+ package com.LevelVini.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
