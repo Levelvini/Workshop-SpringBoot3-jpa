994b2d2a875fdf5a6a90c261f8983e8866ffe8ea
Levelvini <viniciusfc29@gmail.com>
2024-06-11 10:10:55 -0300

    Exception handling-update

--- src/main/java/com/LevelVini/course/services/UserService.java
+++ src/main/java/com/LevelVini/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.LevelVini.course.entities.User;
 import com.LevelVini.course.repositories.UserRepository;
 import com.LevelVini.course.services.exceptions.DataBaseException;
 import com.LevelVini.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id,User obj){
-        User entity = repository.getReferenceById(id);
+       try {
+           User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
+       }catch (EntityNotFoundException e){
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
